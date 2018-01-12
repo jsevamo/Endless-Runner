82aadacd702e565e05af839ab5a88191db051065
Sebastián Vargas <j.sevamo@gmail.com>
2018-01-12 11:25:24 -0500

    Initial commit

---
+++ Assets/Scripts/AddObstacle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObstacle : MonoBehaviour {
+
+	public GameObject obstacle;
+	public List<GameObject> obstacleList = new List<GameObject> ();
+	int a = 0;
+	int timeForRun;
+	public float speed = -0.05f;
+	[SerializeField] int boxCount;
+
+	// Use this for initialization
+	void Start () {
+
+		timeForRun = 999999999;
+		boxCount = 0;
+		StartCoroutine(addObjects());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		moveObjects ();
+	}
+
+	IEnumerator addObjects()
+	{
+		for (int i = 0; i < timeForRun; i++) {
+			yield return new WaitForSeconds (3);
+			//Debug.Log (a);
+			//a++;
+
+			GameObject newObstacle = Instantiate (obstacle, new Vector3 (12, -3.75f, -1), Quaternion.identity) as GameObject;
+			obstacleList.Add (newObstacle);
+			boxCount++;
+		}
+
+	}
+
+
+	void moveObjects()
+	{
+		if (obstacleList != null) {
+			
+			for (int i = 0; i < boxCount; i++) {
+				obstacleList[i].transform.Translate(new Vector2 (speed * Time.deltaTime, 0));
+
+				if (obstacleList[i].transform.position.x < -12) {
+					Destroy (obstacleList [i]);
+					obstacleList.RemoveAt (i);
+					boxCount--;
+
+				}
+			}
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+
+	Rigidbody2D rb;
+	public Vector2 force = new Vector2 (0, 1500);
+	bool IsGrounded;
+	int i;
+	Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		IsGrounded = false;
+		i = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+
+		if (IsGrounded == true) {
+
+			if (Input.GetButtonDown ("Fire1")) {
+				rb.AddForce (force);
+				IsGrounded = false;
+			}
+
+		} else {
+			anim.SetBool ("isJumping", true);
+		}
+
+		//Debug.Log (IsGrounded);
+
+			
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Ground")) {
+			anim.SetBool ("isJumping", false);
+			IsGrounded = true;
+			Debug.Log (IsGrounded);
+
+		} 
+
+		if (other.gameObject.CompareTag ("Obstacle")) {
+			Debug.Log ("You died");
+			Time.timeScale = 0f;
+		} 
+
+	}
+}
