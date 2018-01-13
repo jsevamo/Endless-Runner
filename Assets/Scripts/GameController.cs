eda820cc2881bc37e94374321fd82cec3f64e62d
Sebastián Vargas <j.sevamo@gmail.com>
2018-01-13 16:15:35 -0500

    Added MainMenu screen. Pretty barebones still but works.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
@@ -108,6 +109,9 @@ public class GameController : MonoBehaviour {
 	{
 		wantToTryAgain = true;
 	}
+
+
+
 
 
 }
