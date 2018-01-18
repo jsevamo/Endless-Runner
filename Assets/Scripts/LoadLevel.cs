e12f6903f89672a4127ef1c53e7e41d91ac01c46
Sebastián Vargas <j.sevamo@gmail.com>
2018-01-18 08:13:06 -0500

    Password Implemented. Works but looks tiny at the moment.

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -20,8 +20,11 @@ public class LoadLevel : MonoBehaviour {
 	CharacterScript player;
 	GameController gc;
 	int index;
+	//string password = "";
+	public Font font;
 
 	public string userName;
+	public string passWord;
 
 	// Use this for initialization
 	void Start () {
@@ -30,11 +33,13 @@ public class LoadLevel : MonoBehaviour {
 		gc = GC.GetComponent<GameController> ();
 		
 
-		User1.Add ("j_sevamo");
-		User1.Add ("5");
+		User1.Add ("j_sevamo"); //Username
+		User1.Add ("1"); //Vidas
+		User1.Add ("carne"); //password
 
 		User2.Add ("felipito");
 		User2.Add ("11");
+		User2.Add ("pollo");
 
 		Users.Add (User1);
 		Users.Add (User2);
@@ -43,10 +48,21 @@ public class LoadLevel : MonoBehaviour {
 
 		Sorry.gameObject.SetActive (false);
 
+		InputUsuario.asteriskChar = "$!£%&*"[0];
+
+
 
 
 
 		
+	}
+
+	void OnGUI()
+	{
+		
+		GUI.skin.label.font = GUI.skin.button.font = GUI.skin.box.font = font;
+		GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = 50;
+		passWord = GUI.PasswordField (new Rect (Screen.width / 2 - 343.74f / 2, Screen.height / 2 + 30, 343.74f, 50), passWord, "*" [0], 25);
 	}
 	
 	// Update is called once per frame
@@ -60,13 +76,16 @@ public class LoadLevel : MonoBehaviour {
 
 		string givenName = InputUsuario.text;
 
+
 		for (int i = 0; i < Users.Count; i++) {
-			if (givenName == Users [i][0]) {
+			if (givenName == Users [i][0] && passWord ==  Users [i][2]) {
 				canStart = true;
 				index = i;
 				break;
 			}
 		}
+
+
 
 		if (canStart) {
 			gc.userName = givenName;
